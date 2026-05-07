68cb9133418b3b3e8b20e1d28cec55e854add873
Amir Mehrabi <amir.mehrabi.j@gmail.com>
2026-05-07 17:30:36 +0330

    feat: public entrypoint

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+export { Producer } from './producer.js';
+export { Consumer } from './consumer.js';
+export type {
+  RedisLike,
+  Logger,
+  ProducerOptions,
+  ConsumerOptions,
+  AutoClaimOptions,
+  Handler,
+  ErrorHandler,
+  MessageMeta,
+} from './types.js';
